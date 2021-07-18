644bb9d3d1257470d0d3355acab8b1c354630b5f
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-07-18 21:12:04 +0700

    cleaning up yesterday progress --> bugs and errors

--- src/components/MainContent/Form/Form.jsx
+++ src/components/MainContent/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import {
   TextField,
   Typography,
@@ -10,9 +10,25 @@ import {
   MenuItem,
 } from "@material-ui/core";
 import useStyles from "./styled";
+import moment from "moment";
+import { FinanceManagementContext } from "../../../contexts/globalContext";
+
+const initialState = {
+  amount: "",
+  category: "",
+  type: "Income",
+  date: moment().locale("de").format("LLLL"),
+};
 
 export const Form = () => {
+  const [formData, setFormData] = useState(initialState);
+  const { addTransaction } = useContext(FinanceManagementContext);
   const classes = useStyles();
+
+  const createTransaction = () => {};
+
+  console.log(formData);
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -23,7 +39,12 @@ export const Form = () => {
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Type</InputLabel>
-          <Select>
+          <Select
+            value={formData.type}
+            onChange={({ target }) =>
+              setFormData({ ...formData, type: target.value })
+            }
+          >
             <MenuItem value="Income">Income</MenuItem>
             <MenuItem value="Expense">Expense</MenuItem>
           </Select>
@@ -32,23 +53,45 @@ export const Form = () => {
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Category</InputLabel>
-          <Select>
+          <Select
+            value={formData.category}
+            onChange={({ target }) =>
+              setFormData({ ...formData, category: target.value })
+            }
+          >
             <MenuItem value="business">Business</MenuItem>
             <MenuItem value="salary">Salary</MenuItem>
           </Select>
         </FormControl>
       </Grid>
       <Grid item xs={6}>
-        <TextField type="number" label="Amount" fullWidth />
+        <TextField
+          type="number"
+          label="Amount"
+          fullWidth
+          value={formData.amount}
+          onChange={({ target }) =>
+            setFormData({ ...formData, amount: target.value })
+          }
+        />
       </Grid>
       <Grid item xs={6}>
-        <TextField type="date" label="Date" fullWidth />
+        <TextField
+          type="date"
+          label="Date"
+          fullWidth
+          value={formData.date}
+          onChange={({ target }) =>
+            setFormData({ ...formData, date: target.value })
+          }
+        />
       </Grid>
       <Button
         variant="outlined"
         className={classes.button}
         color="primary"
         fullWidth
+        onClick={createTransaction}
       >
         Create
       </Button>
